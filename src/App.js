f8ffcb9e927a20f523dacbf1e5d61fafea5f5343
Chris-Kay <chris.kay01@bbc.co.uk>
2018-07-26 15:01:34 +0100

    Firebase talking to backend

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ class App extends Component {
 
     this.state = {
       message: '',
-      username: ''
+      username: '',
+      showThankYou: false
     }
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this); // <-- add this line
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   
@@ -24,6 +25,12 @@ class App extends Component {
       message: this.state.message,
       user: this.state.username
     }
+
+    this.setState({
+      message: '',
+      username: '',
+      showThankYou: true
+    });
     itemsRef.push(item);
   }
 
@@ -34,20 +41,29 @@ class App extends Component {
   }
 
   render() {
+
+    if(this.state.showThankYou) {
+      return (
+        <div>
+          <h1> Thank You, your message is with the editorial team </h1>
+        </div>
+      )
+    }
+
     return (
       <div className='app'>
         <header>
             <div className='wrapper'>
-              <h1>Fun Food Friends</h1>
+              <h1>Get Involved</h1>
               
             </div>
         </header>
         <div className='container'>
           <section className='add-item'>
-          <form onSubmit={this.handleSubmit}>
-            <input type="text" name="username" placeholder="Joe Blogs" onChange={this.handleChange} value={this.state.username} />
-            <input type="text" name="message" placeholder="" onChange={this.handleChange} value={this.state.message} />
-            <button>Add Item</button>
+          <form className="get-involved-form" onSubmit={this.handleSubmit}>
+            <input className="username" type="text" name="username" placeholder="Joe Blogs" onChange={this.handleChange} value={this.state.username} />
+            <textarea className="input-box" align="top" type="text" name="message" placeholder="" onChange={this.handleChange} value={this.state.message} />
+            <button className="send-button">Send</button>
         </form>
           </section>
           <section className='display-item'>
